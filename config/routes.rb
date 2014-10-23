de1c4454d3fedfba8a8fec0817dcb4f75a83692e
Ile Eftimov <ileeftimov@gmail.com>
2014-10-23 10:52:42 +0200

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Shortenizer::Engine.routes.draw do
+  root 'u#new'
   match '/shorten', to: 'u#new', via: :get, as: :new_short_link
   match '/create', to: 'u#create', via: :post, as: :create_short_link
   match '/:shortlink', to: 'u#index', via: :get
