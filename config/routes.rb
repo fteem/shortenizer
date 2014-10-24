dc1c66c964969068e9c05e4ba38931d8b67edddd
Ile Eftimov <ileeftimov@gmail.com>
2014-10-24 22:07:57 +0200

    add specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Shortenizer::Engine.routes.draw do
   root 'u#new'
+
   match '/shorten', to: 'u#new', via: :get, as: :new_short_link
   match '/create', to: 'u#create', via: :post, as: :create_short_link
   match '/:shortlink', to: 'u#index', via: :get
---
+++ spec/models/shortenizer/url_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe Shortenizer::Url do
+  describe '#append_https' do
+    it 'appends https if link does not begin with http' do
+      url = described_class.create(shortlink: '12345', target: 'yahoo.com')
+      url.reload
+
+      expect(url.target).to eq 'https://yahoo.com'
+    end
+  end
+
+  describe '#create_unique_identifier' do
+    it 'sets the url shortlink to a unique string' do
+      expect(SecureRandom).to receive(:hex).and_return "12345"
+      url = described_class.create(target: 'http://eftimov.net')
+      url.reload
+
+      expect(url.shortlink).to eq "12345"
+    end
+  end
+
+end
